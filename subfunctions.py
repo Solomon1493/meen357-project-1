d09680158f9f1d399e68d80aa019c317f8110fe4
Solomon Moon <solomonjmoon@gmail.com>
2026-09-17 11:27:20 -0500

    finalized graphs_sr

--- subfunctions.py
+++ subfunctions.py
@@ -30,9 +30,9 @@ def define_planet():
 def tau_dcmotor():
     return tau_dcmotor
 
-def get_gear_ratio(speed_reducer):
-    pinion_d = speed_reducer["diam_pinion"]
-    gear_d = speed_reducer["diam_gear"]
+def get_gear_ratio(wheel_assembly):
+    pinion_d = wheel_assembly['speed_reducer']["diam_pinion"]
+    gear_d =  wheel_assembly['speed_reducer']["diam_gear"]
     ratio = (gear_d/pinion_d)**2
     return ratio
 
@@ -52,9 +52,3 @@ def F_drive():
     return F_drive
 def F_net():
     return F_net
-
-roverspecs = define_rover()
-print(roverspecs)
-
-mass_rover = get_mass(roverspecs['wheel_assembly'], roverspecs['chassis'], roverspecs['science_payload'], roverspecs['power_subsys'])
-print(mass_rover)
