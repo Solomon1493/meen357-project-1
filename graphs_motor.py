d6fda7f9204cfee41295181f78ba7ad742cb146e
Solomon Moon <solomonjmoon@gmail.com>
2026-09-17 10:35:48 -0500

    graphs_motor complete

--- graphs_motor.py
+++ graphs_motor.py
@@ -19,5 +19,27 @@ rover = define_rover()
 print(rover)
 motor = rover["wheel_assembly"]["motor"]
 
-tau = tau_dcmotor(omega_array, motor)
+omega_array = np.linspace(0, 100, 100)
 
+tau_array = tau_dcmotor(omega_array, motor)
+power_array = power_dcmotor(omega_array, tau_array)
+
+plt.subplot(3,1,1)
+plt.plot(tau_array, omega_array)
+plt.xlabel('Torque [Nm]')
+plt.ylabel('Speed [rad/s]')
+plt.title('Motor Shaft Speed vs. Motor Shaft Torque')
+
+plt.subplot(3,1,2)
+plt.plot(tau_array, power_array)
+plt.xlabel('Torque [Nm]')
+plt.ylabel('Power [W]')
+plt.title('Motor Power vs. Motor Shaft Torque')
+
+plt.subplot(3,1,3)
+plt.plot(omega_array, power_array)
+plt.xlabel('Speed [rad/s]')
+plt.ylabel('Power [W]')
+plt.title('Motor Power vs. Motor Shaft Speed')
+
+plt.show()
